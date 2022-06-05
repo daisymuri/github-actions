eb453feafa4fe978ad315f656306610e20ba8164
daisymuri <daisy21murillo@gmail.com>
2022-06-05 16:37:23 -0700

    testing done

--- binary_search.py
+++ binary_search.py
@@ -16,3 +16,7 @@ def binary_search(list, answer):
     return None
 
 my_list = [1, 3, 5, 7, 9]
+
+print(binary_search(my_list, 3))
+print(binary_search(my_list, 9))
+print(binary_search(my_list, 5))
